0671bbf8d295a424c3bca49edf144b0060e64bd6
Antoine Latter <aslatter@gmail.com>
2010-02-14 06:36:19 +0000

    give bryan credit

--- Data/SmallArray.hs
+++ Data/SmallArray.hs
@@ -1,6 +1,25 @@
-{-# LANGUAGE CPP #-}
-{-# LANGUAGE Rank2Types #-}
-
+-- |
+-- Module      : Data.SmallArray
+-- Copyright   : (c) Bryan O'Sullivan 2009
+--               (c) Antoine Latter 2010
+--
+-- License     : BSD-style
+-- Maintainer  : aslatter@gmail.com
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Packed, unboxed, heap-resident arrays.  Suitable for performance
+-- critical use, both in terms of large data quantities and high
+-- speed.
+--
+-- This module is intended to be imported @qualified@, to avoid name
+-- clashes with "Prelude" functions, e.g.
+--
+-- > import qualified Data.SmallArray as A
+--
+-- The names in this module resemble those in the 'Data.Array' family
+-- of modules, but are shorter due to the assumption of qualifid
+-- naming.
 module Data.SmallArray
     (
     -- * Array types
