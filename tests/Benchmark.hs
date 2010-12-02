cd79cd8b76fa72232faa5c946c28dfab365db0fd
Antoine Latter <aslatter@gmail.com>
2010-12-02 02:36:00 +0000

    remove storable vector from benchmark

--- tests/Benchmark.hs
+++ tests/Benchmark.hs
@@ -7,8 +7,6 @@ import qualified Data.ByteString.Internal as BS
 
 import qualified Data.Array.Unboxed as UA
 
-import qualified Data.StorableVector as SV
-
 import qualified Data.SmallArray as SA
 
 import Criterion.Main
@@ -17,37 +15,31 @@ main = defaultMain
        [ bgroup "Word8" $
           let bs = BS.pack word8s
               uarr = mkUArray8s word8sAssocs
-              sv = SV.pack word8s
               sa = SA.fromList word8s
           in 
            [ bgroup "pack"
                [ bench "bytestring"   $ nf BS.pack word8s
                , bench "uarray"       $ nf mkUArray8s word8sAssocs
-               , bench "storable-vec" $ nf SV.pack word8s
                , bench "small-array"  $ nf SA.fromList word8s
                ]
            , bgroup "unpack"
                [ bench "bytestring"   $ nf BS.unpack bs
                , bench "uarray"       $ nf UA.elems uarr
-               , bench "storable-vec" $ nf SV.unpack sv
                , bench "small-array"  $ nf SA.toList sa
                ]
            , bgroup "eq"
                [ bench "bytestring"   $ nf (bs ==) bs
                , bench "uarray"       $ nf (uarr ==) uarr
-               , bench "storable-vec" $ nf (sv ==) sv
                , bench "small-array"  $ nf (sa ==) sa
                ]
            ]
        , bgroup "Word16"
            [ bgroup "pack"
                [ bench "uarray"      $ nf mkUArray16s word16sAssocs
-               , bench "storable-vec" $ nf SV.pack word16s
                , bench "small-array" $ nf SA.fromList word16s
                ]
            , bgroup "unpack"
                [ bench "uarray"      $ nf UA.elems (mkUArray16s word16sAssocs)
-               , bench "storable-vec" $ nf SV.unpack (SV.pack word16s)
                , bench "small-array" $ nf SA.toList (SA.fromList word16s)
                ]
            ]
@@ -83,4 +75,4 @@ word16sAssocs = zip [1..length word16s] word16s
 
 instance NFData BS.ByteString where
 instance NFData (UA.UArray i e)
-instance NFData (SV.Vector e)
+
